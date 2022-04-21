fa221499c29b3b6034e8a1a13ce34be5f898ee70
RAj Patel <rajphx299@gmail.com>
2022-04-21 15:15:17 +0530

    intial

--- StackInt.java
+++ StackInt.java
@@ -1,102 +1,158 @@
 public class StackInt {
-    public int[] arr = new int[5] ;
-    private int top = -1 ;
-    int b[] ;
-    int c[] ;
+	int []arr;
+	int top;
+	int sz1,sz2;
+	
+	StackInt(){
+		arr = new int[10];
+		top = -1;
+	}
+	
+	StackInt(int sz){
+		arr = new int[sz];
+		top = -1;
+	}
+	
+	public void print() {
+		
+		if(top == -1) {
+			System.out.println("The stack is empty");
+		}
+		
+		for(int i = 0; i <= top; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	
+	public void push(int item) {
+		
+		if(top == arr.length - 1) {
+			System.out.println("Stack OverFlow");
+			return;
+		}
+		arr[++top] = item;
+	}
+	
+	public int getTop() {
+		
+		return arr[top];
+	}
+	
+	public int pop() {
+		
+		if(top == -1) {
+			System.out.println("Can't PoP");
+			return -1;
+		}
+		return arr[top--];
+	}
+	
+	public int peek() {
+		if(top == -1) {
+			System.out.println("Can't Peek");
+			return -1;
+		}
+		else {
+			return arr[top];
+		}
+	}
+	
+	public boolean equals(StackInt si2) {
+		
+		if(this.top != si2.top) {
+			return false;
+		}
+		else {
+			while(top != -1) {
+				if(this.arr[top] != si2.arr[top]) {
+					return false;
+				}
+				top--;
+			}
+			return true;
+		}
+		
+	}
+	
+	public int getMinElement() {
+		
+		int Min = 99999999; 
+		if(top == -1) {
+			System.out.println("The stack is empty");
+			return -1;
+		}
+		else {
+			while(top != -1) {
+				if(this.peek() < Min ) {
+					Min = this.pop();
+					top--;
+				}
+			}
+			return Min;
+		}
+	}
+	
+	public StackInt copyStack() {
+		StackInt newStack = new StackInt(this.arr.length);
+		StackInt duplicate = new StackInt(this.arr.length);
+		
+		while(!this.isEmpty()) {
+			newStack.push(this.peek());
+			duplicate.push(this.pop());
+		}
+		
+		while(!duplicate.isEmpty()) {
+			this.push(duplicate.pop());
+		
+		}
+		
+		while(!newStack.isEmpty()) {
+			duplicate.push(newStack.pop());
+		
+		}
+		
+		return duplicate;
+		
+	}
+	
+	public int deleteMiddle() {
+		StackInt temp = new StackInt(top);
+		int middle = top/2;
+		
+		while(this.top > middle) {
+			temp.push(this.pop());
+		}
+		int deletedElement = this.pop();
+		
+		while(temp.top != -1) {
+			this.push(temp.pop());
+		}
 
-    public int[] getArr() {
-        return this.arr;
-    }
-
-    public void setArr(int[] arr) {
-        this.arr = arr;
-    }
-
-    public int getTop() {
-        return this.top;
-    }
-
-    public void setTop(int top) {
-        this.top = top;
-    }
-
-    public StackInt() {
-        arr = new int[10];
-        top = -1; }
-
-    public StackInt(int sz) {
-        arr = new int[sz]; 
-        top = -1;
-        }
-    
-    public void print() {
-          for (int i = 0; i < top+1; i++) {
-              System.out.print(arr[i] + "    ");
-          }
-        }
-    public void push(int item) {
-        if ((top +1)==arr.length) {
-            System.out.println("The Stack is full");
-        }
-        else {
-            top ++ ;
-            arr[top] = item ;
-            System.out.println(item + " pushed into stack");
-            }
-        }
-    public int getop() {
-        return arr[top] ;
-    }   
-    public int pop() { 
-        if (top==-1) {
-            System.out.println("\nStack is empty\n");
-            return 0 ;
-        }
-        else{
-        top--;
-        System.out.println("\nElement popped\n");
-        return 1 ;
-         }
-    }
-    public int peek (){
-        if (top==-1) {
-            System.out.println("Stack is empty");
-            return 0 ;
-        }
-        else{
-        return arr[top] ;}
-    }
-    public void equal(){
-        
-    }
-    public void getminElement()
-    {
-        int x = arr[0];
-        for (int i = 0; i < top; i++) {
-            if (arr[i]<x) {
-                x = arr[i];
-            }
-        }
-        System.out.println(x);
-    }
-    public void copyStack() {
-        int b[] = arr.clone();
-        for (int i = 0; i < b.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
-    public void rev()
-    {
-        int z = 0 ;
-        int c[] = arr.clone();
-        for (int i = arr.length-1; i>=0 ;--i) {
-            c[z] = arr[i];
-            z++;
-        } 
-        for (int i = 0; i < c.length; i++) {
-            System.out.println(c[i]); ;
-        }     
-    }
+		return deletedElement;
+		
+	}
+	
+	public boolean isEmpty() {
+		return top == -1;
+	}
+	
+	public StackInt sort() {
+		StackInt tempStack = new StackInt(this.arr.length);
+		while(!(this.isEmpty())){
+			if(tempStack.isEmpty()) {
+				tempStack.push(this.pop());
+			}
+			else {
+				int element = this.pop();  
+				while(tempStack.top !=-1 && (tempStack.peek() > element)) {
+					this.push(tempStack.pop());
+				}
+				tempStack.push(element);
+			}
+		}
+		
+		return tempStack;
+	}
+	
 }
- 
-        
--- test.java
+++ test.java
@@ -1,10 +1,16 @@
-public class test {
-    public static void main(String[] args) {
-        
-        StackInt si1 = new StackInt();
-        si1.push(100);
-        si1.push(200); 
-        si1.rev();
-        si1.print();
-    }
+public class Test {
+	public static void main(String[] args) {
+		StackInt si = new StackInt();    
+		si.push(300);      si.push(200); 
+		si.push(100);      si.push(700); 
+		si.push(600);
+		
+		System.out.println("Orginal Stack: ");
+		si.print();
+		
+		System.out.println("Duplicate Stack: ");
+		StackInt duplicateStack = si.copyStack();
+		duplicateStack.print();
+		
+	}
 }
